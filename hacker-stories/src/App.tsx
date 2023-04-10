9445ee85b9a424065b4574e4222f3185e52f11c0
ch <mkmsacc@outlook.com>
2023-04-11 00:31:37 +0700

    39

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -28,18 +28,23 @@ function App() {
             <input type="text" id="search"/>
 
             <hr/>
+            <List/>
+        </div>
+    );
+}
 
-            <ul>
-                {list.map(item => <li key={item.objectID}>
+function List() {
+    return (
+        <ul>
+            {list.map(item => <li key={item.objectID}>
                     <span>
                         <a href={item.url}>{item.title}</a>
                     </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                </li>)}
-            </ul>
-        </div>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </li>)}
+        </ul>
     );
 }
 
