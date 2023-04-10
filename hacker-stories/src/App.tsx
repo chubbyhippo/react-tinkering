f0b3e91a9051be6dd3c696d7abaaad4024554c84
ch <mkmsacc@outlook.com>
2023-04-10 22:43:38 +0700

    38

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -1,20 +1,37 @@
 import * as React from "react";
 
-function getTitle(title: string) {
-    return title;
-}
-
-const test = "test"
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
 function App() {
     return (
         <div>
-            <h1>
-                Hello {getTitle('React')}
-            </h1>
+            <h1>My Hacker Stories</h1>
 
             <label htmlFor="search">Search: </label>
             <input type="text" id="search"/>
+
+            <hr/>
+
+            <ul>
+                {list.map(item => <li>{item.title}</li>)}
+            </ul>
         </div>
     );
 }
